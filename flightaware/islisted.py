65430472a3ff585be716448314bd7684cc423246
David M Walker <davidw@datamgmt.com>
2024-07-20 18:12:27 +0100

    Added islisted script

---
+++ flightaware/islisted.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+def value_exists_in_file(value, filename='data.txt'):
+    try:
+        with open(filename, 'r') as file:
+            for line in file:
+                if line.strip() == value:
+                    return True
+        return False
+    except FileNotFoundError:
+        print(f"The file {filename} does not exist.")
+        return False
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python check_value.py <value_to_check>")
+        sys.exit(2)
+
+    value_to_check = sys.argv[1]
+    filename = '/Users/datamgmt/Documents/GitHub/pi-code/flightaware/aircraft.txt'
+
+    if value_exists_in_file(value_to_check, filename):
+        #print(f"The value '{value_to_check}' exists in the file '{filename}'.")
+        sys.exit(1)
+    else:
+        #print(f"The value '{value_to_check}' does not exist in the file '{filename}'.")
+        sys.exit(0)
+
