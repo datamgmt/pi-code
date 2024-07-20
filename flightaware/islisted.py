fbb7578ae5d66e302e54205bdbbc2ff92316cf5c
David M Walker <davidw@datamgmt.com>
2024-07-20 18:43:09 +0100

    Update islisted.py

--- flightaware/islisted.py
+++ flightaware/islisted.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-
 def value_exists_in_file(value, filename='data.txt'):
     try:
         with open(filename, 'r') as file:
@@ -11,23 +9,19 @@ def value_exists_in_file(value, filename='data.txt'):
                     return True
         return False
     except FileNotFoundError:
-        print(f"The file {filename} does not exist.")
+        print(f'The file {filename} does not exist.')
         return False
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("Usage: python check_value.py <value_to_check>")
+        print('Usage: python check_value.py <value_to_check>')
         sys.exit(2)
 
     value_to_check = sys.argv[1]
     filename = '/home/pi/pi-code/flightaware/aircraft.txt'
 
     if value_exists_in_file(value_to_check, filename):
-        #print(f"The value '{value_to_check}' exists in the file '{filename}'.")
-        print("1")
-        #sys.exit(1)
+        print('1', end='')
     else:
-        #print(f"The value '{value_to_check}' does not exist in the file '{filename}'.")
-        print("0")
-        #sys.exit(0)
-
+        print('0', end='')
+        
